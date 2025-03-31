bdea38515e061be5a2aaef32e4e0061a353e018f
Twalsh26 <126Twalsh@frhsd.com>
2025-03-31 12:51:55 -0400

    Done5

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll(".card")
+
+
+function flipCard(e){
+    console.log(e.target);
+}
+
+
+cards.forEach(card => {
+card.addEventListener("click" , flipCard);
+});
